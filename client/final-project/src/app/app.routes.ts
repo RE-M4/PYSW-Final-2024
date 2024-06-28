04ee144c3400b43fe637f5039a5235bf0984b0de
sofi <abrilyanez123@gmail.com>
2024-06-28 14:48:45 -0300

    creación de los componentes de registro y logueo de usuarios , se trabajó en la estructura(html) y estilo(css)

--- client/final-project/src/app/app.routes.ts
+++ client/final-project/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { CartillaMedicaComponent } from './components/cartilla-medica/cartilla-medica.component';
 import { TurnoRegistroComponent } from './components/turno-registro/turno-registro.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 export const routes: Routes = [
     {path:'cartilla-medica', component: CartillaMedicaComponent},
-    {path:'turno-registro',component: TurnoRegistroComponent}
-    
+    {path:'turno-registro',component: TurnoRegistroComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule] 
+})
+export class AppRoutingModule { }
