8284ad9fec41f360b0b0c22ccc522599081cac9b
agustin castellon <agustincastellon06@gmail.com>
2024-06-30 13:35:25 -0300

    Creacion de CRUD para medico y novedades

--- server/controllers/novedades.controller.js
+++ server/controllers/novedades.controller.js
@@ -2,11 +2,19 @@ const Novedades = require('../models/novedades');
 
 const controlNovedades = {}
 
-controlNovedades.getNovedades = async (req, res) => { 
+// Obtener todas las novedades
+controlNovedades.getNovedades = async (req, res) => {
     var listaa = await Novedades.find();
     res.json(listaa);
 }
 
+// Obtener novedades por estado "pendiente" o "procesado" 
+controlNovedades.getNovedadByEstado = async(req, res) =>{
+    var listaa = await Novedades.find({estado: req.params.estado});
+    res.json(listaa);
+}
+
+// Crear una nueva novedad
 controlNovedades.createNovedades = async (req, res) => {
     var nove = new Novedades(req.body);
     try {
@@ -15,11 +23,58 @@ controlNovedades.createNovedades = async (req, res) => {
             'status': '1',
             'msg': 'Novedades guardado.'
         })
-    } catch (error) { console.log(error); 
+    } catch (error) {
+        console.log(error);
         res.status(400).json({
             'status': '0',
             'msg': 'Error_Alta_Novedades.'
         })
+    }
+}
+
+// Obtener novedad por ID
+controlNovedades.getNovedadById = async (req, res) => {
+    try {
+        let novedad = await Novedades.findById(req.params.id);
+        res.json(novedad);
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error_Al_buscar_novedad.'
+        })
+    }
+}
+
+// Editar una novedad por ID
+controlNovedades.editNovedad = async (req, res) => {
+    let novedad = new Novedades(req.body);
+    try {
+        await Novedades.updateOne({_id: req.body._id}, novedad)
+        res.json({
+            'status': '1',
+            'msg': 'Novedad editada.'
+        });
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error_Al_editar_novedad.'
+        })
+    }
+}
+
+// Eliminar una novedad por ID
+controlNovedades.deleteNovedad = async (req, res) =>{
+    try {
+        await Novedades.deleteOne({_id: req.params.id});
+        res.json({
+            'status': '1',
+            'msg': 'Novedad eliminada.'
+        });
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error_Al_eliminar_novedad.'
+        });
     }
 }
 
--- server/routes/medico.route.js
+++ server/routes/medico.route.js
@@ -5,10 +5,11 @@ const controlMedico = require('../controllers/medico.controller');
 const router = expreess.Router();
 //definimos las rutas 
 router.get('/', controlMedico.getMedico);
+router.get('/:id', controlMedico.getMedicoById);
 router.post('/', controlMedico.createMedico);
-//router.get('/suma', controlll.getConverttt);
-//router.get('/emaill/:email', controlll.getBuscaEmail);  
-//router.get('/mOrigenn/:origen/mDestinoo/:destino', controlll.getBuscaM);  
+router.put('/:id', controlMedico.editMedico);
+router.delete('/:id', controlMedico.deleteMedico);
+router.get('/medicos/:especialidad', controlMedico.getMedicoByEspecialidad)
 
 //exportamos el modulo de rutas
 module.exports = router;
